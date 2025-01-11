b36545f8c66cc2b8e79d378173e095a893c658a8
Håkon Hægland <hakon.hagland@gmail.com>
2025-01-11 18:22:28 +0100

    Temp

--- debug_python_click/app.py
+++ debug_python_click/app.py
@@ -1,12 +1,20 @@
 import click
 import pdb
 
-class DebugCommand(click.Command):
-    def format_help(self, ctx, formatter):
-        pdb.set_trace()  # Breakpoint here
-        return super().format_help(ctx, formatter)
+# Save the original get_help method
+original_get_help = click.core.Command.get_help
 
-@click.command(cls=DebugCommand)
+# Define a patched version
+def patched_get_help(self, ctx):
+    print("DEBUG: Entering get_help()")
+    result = original_get_help(self, ctx)
+    print("DEBUG: Exiting get_help()")
+    return result
+
+# Apply the monkey patch
+click.core.Command.get_help = patched_get_help
+
+@click.command()
 @click.option('--name', default="World", help='Who to greet')
 def greet(name):
     click.echo(f"Hello, {name}!")
