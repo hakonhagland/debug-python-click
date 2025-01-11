6aad3b9e0c3a077fc6455ba8e664d5697a9917e2
Håkon Hægland <hakon.hagland@gmail.com>
2025-01-11 18:21:02 +0100

    Temp

--- debug_python_click/app.py
+++ debug_python_click/app.py
@@ -1,6 +1,12 @@
 import click
+import pdb
 
-@click.command()
+class DebugCommand(click.Command):
+    def format_help(self, ctx, formatter):
+        pdb.set_trace()  # Breakpoint here
+        return super().format_help(ctx, formatter)
+
+@click.command(cls=DebugCommand)
 @click.option('--name', default="World", help='Who to greet')
 def greet(name):
     click.echo(f"Hello, {name}!")
